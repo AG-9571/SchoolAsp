761b10218cb719b806e658eba6a3528cab574223
Angel Ortega <codenode.ortega@gmail.com>
2023-01-17 06:01:42 -0600

    segundo commit

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -1,7 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace school.Models;
 
 public class Admin : objectBaseModel
 {
-    
+    public string? idSchool {get; set;}
+    [Required]
+    public string? pass {get; set;}
+    [Required]
+    public string? email {get; set;}
+    public int code {get; set;}
+
 }
 
